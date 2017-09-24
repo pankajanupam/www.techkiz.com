95cfe4a7434c1a584b6f884254f0e0e643b80b0c
Pankaj Anupam <mymail.anupam@gmail.com>
2017-09-24 05:51:33 +0530

    Use DB and Create table

--- nodejs/nodejs-mysql/mysql.db.js
+++ nodejs/nodejs-mysql/mysql.db.js
@@ -1,19 +1,20 @@
 var mysql = require('mysql');
- 
+
 var connection = mysql.createConnection({
   host: "localhost",
   user: "your username",
-  password: "your password"
+  password: "your password",
+  database: "database_name"
 });
- 
+
 connection.connect(function(err) {
- 
+
   if (err) throw err;
-  console.log("Connected Database");
- 
-  connection.query("CREATE DATABASE database_name", function (err, result) {
+  console.log("Database Connected");
+
+  connection.query("CREATE TABLE user (name VARCHAR(255), email VARCHAR(255))", function (err, result) {
     if (err) throw err;
-    console.log("Database created");
+    console.log("Table created");
   });
- 
+  
 });
